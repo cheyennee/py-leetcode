8892245b3afd19967d922af0e6c0e529d41eb1be
Cheyennee <17362995059@163.com>
2020-11-10 09:54:00 +0800

    2020/11/09

--- 35.py
+++ 35.py
@@ -1,5 +1,10 @@
+
 '''
-解法一：内存消耗大，速度快96
+本题易混点：1.循环结束条件:right>left 2.在nums[mid] == target可以不用判断
+'''
+
+'''
+解法一：暴力法：内存15.55,运行时间66.97
 '''
 class Solution(object):
     def searchInsert(self, nums, target):
@@ -16,9 +21,15 @@ class Solution(object):
                 return i
         return length
 
+'''
+增加了target == nums[mid]判断之后，若数组元素有重复，可能会出错
+例如：[1,1,1,1,1]
+增加此判断条件后，返回2，但实际应该返回0
+'''
+
 
 '''
-解法二：想采用二分法，但是代码写得太烂了，导致执行时间41.09，内存消耗5.10
+解法二：想采用二分法，但是代码写得太烂了，导致执行时间41.09，内存5.10
 '''
 class Solution(object):
     def searchInsert(self, nums, target):
@@ -39,8 +50,7 @@ class Solution(object):
         return left
 
 '''
-解法三：标准的二分法，可是执行时间：41.09，内存消耗：12.38
-所以是因为py天生慢？
+解法三：在解法二的基础上修改了，执行时间：41.09，内存：12.38
 '''
 class Solution(object):
     def searchInsert(self, nums, target):
@@ -55,3 +65,20 @@ class Solution(object):
             elif (nums[mid] < target):
                 left = mid + 1
         return left
+
+'''
+解法四：解法二的改进版，执行时间：96.8,内存：9.1
+'''
+class Solution(object):
+    def searchInsert(self, nums, target):
+        left = 0
+        right = len(nums) - 1
+        while(left <= right):
+            mid = int((left + right) / 2)
+            if nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+        return left
+
+
