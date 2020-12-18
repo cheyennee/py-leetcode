851292b781f2f3f1a83047d025d7f52434626d46
Cheyennee <17362995059@163.com>
2020-12-18 09:11:00 +0800

    2020/12/18

--- 118.py
+++ 118.py
@@ -14,3 +14,22 @@ class Solution(object):
             result.append(temp)
         return result
 
+#解法二：递归，时间92，内存16
+#但是我没有看懂递归QAQ
+class Solution(object):
+    def generate(self, numRows):
+        if numRows == 0:
+            return []
+        if numRows == 1:
+            return [[1]]
+        pre = self.generate(numRows - 1)
+        last = pre[-1]
+        i = 0
+        next = [1]
+        while i < len(last) - 1:
+            next.append(last[i] + last[i+1])
+            i += 1
+        next.append(1)
+        pre.append(next)
+        return pre
+
